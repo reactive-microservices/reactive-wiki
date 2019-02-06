31c58df05938bbac563079e7da1151df32a81665
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-02-06 13:09:54 +0200

    Separate DB connection and http server functions added.

---
+++ src/main/java/com/max/reactive/wiki/PageDao.java
@@ -0,0 +1,13 @@
+package com.max.reactive.wiki;
+
+public class PageDao {
+
+    public static final String SQL_CREATE_PAGE_TABLE = "create table if not exists PAGE (id integer identity primary key, " +
+            "name varchar(255) unique, content clob)";
+    public static final String SQL_GET_PAGE = "select id, content from PAGE where name = ?";
+    public static final String SQL_CREATE_PAGE = "insert into PAGE values (NULL, ?, ?)";
+    public static final String SQL_SAVE_PAGE = "update PAGE set content = ? where id = ?";
+    public static final String SQL_ALL_PAGES = "select name from PAGE";
+    public static final String SQL_DELETE_PAGE = "delete from PAGE where id = ?";
+
+}
--- src/main/java/com/max/reactive/wiki/WikiVerticle.java
+++ src/main/java/com/max/reactive/wiki/WikiVerticle.java
@@ -1,6 +1,10 @@
 package com.max.reactive.wiki;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,13 +16,66 @@ public class WikiVerticle extends AbstractVerticle {
 
     static final int PORT = 8080;
 
+    private JDBCClient dbClient;
+
     @Override
-    public void start() {
-        
+    public void start(Future<Void> startFuture) {
+        Future<Void> initSteps = prepareDatabase().compose(v -> startHttpServer());
+        initSteps.setHandler(startFuture.completer());
+    }
+
+    private Future<Void> prepareDatabase() {
+        Future<Void> databaseFuture = Future.future();
+
+        dbClient = JDBCClient.createShared(vertx,
+                                           new JsonObject().
+                                                   put("url", "jdbc:hsqldb:file:db/wiki").
+                                                   put("driver_class", "org.hsqldb.jdbcDriver").
+                                                   put("max_pool_size", 30));
+
+        dbClient.getConnection(ar -> {
+            if (ar.failed()) {
+                databaseFuture.fail(ar.cause());
+            }
+            else {
+
+                SQLConnection conn = ar.result();
+
+                conn.execute(PageDao.SQL_CREATE_PAGE_TABLE, createResult -> {
+                    conn.close();
+
+                    if (createResult.failed()) {
+                        LOG.error("Can't create PAGE table", createResult.cause());
+                        databaseFuture.fail(createResult.cause());
+                    }
+                    else {
+                        LOG.info("Connection to DB successfully created.");
+                        databaseFuture.complete();
+                    }
+                });
+
+
+            }
+        });
+
+        return databaseFuture;
+    }
+
+    private Future<Void> startHttpServer() {
+        Future<Void> httpServerFuture = Future.future();
+
         vertx.createHttpServer()
                 .requestHandler(req -> req.response().end("Hello wiki."))
-                .listen(PORT);
+                .listen(PORT, ar -> {
+                    if (ar.failed()) {
+                        httpServerFuture.fail(ar.cause());
+                    }
+                    else {
+                        LOG.info("HTTP server successfulyl started at port {}", PORT);
+                        httpServerFuture.complete();
+                    }
+                });
 
-        LOG.info("MainVerticle started at port {}", PORT);
+        return httpServerFuture;
     }
 }

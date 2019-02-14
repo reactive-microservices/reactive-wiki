131fd0c8fdf2ee36eaae5d9909c22728502d0ad0
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-02-14 15:30:54 +0200

    RxJava 2 added.

--- src/main/java/com/max/reactive/wiki/MainVerticle.java
+++ src/main/java/com/max/reactive/wiki/MainVerticle.java
@@ -1,6 +1,5 @@
 package com.max.reactive.wiki;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import org.slf4j.Logger;
@@ -8,27 +7,24 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
-public class MainVerticle extends AbstractVerticle {
+public class MainVerticle extends io.vertx.reactivex.core.AbstractVerticle {
 
     private static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Override
+    @SuppressWarnings("CheckReturnValue")
     public void start(Future<Void> startFuture) {
 
         final int wikiVerticlesCount = Runtime.getRuntime().availableProcessors();
 
         LOG.info("Deploying {} WikiVerticles", wikiVerticlesCount);
 
-        vertx.deployVerticle(WikiVerticle.class,
-                             new DeploymentOptions().setInstances(wikiVerticlesCount),
-                             ar -> {
-                                 if (ar.failed()) {
-                                     LOG.error("Can't properly deploy few WikiVerticle from MainVerticle", ar.cause());
-                                     startFuture.fail(ar.cause());
-                                 }
-                                 else {
-                                     startFuture.complete();
-                                 }
-                             });
+        vertx.rxDeployVerticle(WikiVerticle.class.getCanonicalName(),
+                               new DeploymentOptions().setInstances(wikiVerticlesCount)
+        ).subscribe(ar -> startFuture.complete(),
+                    error -> {
+                        LOG.error("Can't properly deploy few WikiVerticle from MainVerticle", error);
+                        startFuture.fail(error);
+                    });
     }
 }
